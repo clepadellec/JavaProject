556a0fb1786ba771ea6c836843b592e7d918877f
Charlie <Charlie@Charlie>
2019-12-30 02:03:39 +0100

    nouvelle classe

---
+++ MesTweets/src/utilisateur.java
@@ -0,0 +1,34 @@
+public class utilisateur {
+
+	private String u_pseudo_users = "";
+	private Integer u_nombre_retweet = 0;
+	private Integer u_nombre_mention = 0;
+	
+	public utilisateur(String u_pseudo_users, Integer u_nombre_retweet, Integer u_nombre_mention) {
+		this.u_pseudo_users = u_pseudo_users;
+		this.u_nombre_retweet = u_nombre_retweet;
+		this.u_nombre_mention = u_nombre_mention;
+	}
+	
+
+	public String getU_pseudo_users() {
+		return u_pseudo_users;
+	}
+	public void setU_pseudo_users(String u_pseudo_users) {
+		this.u_pseudo_users = u_pseudo_users;
+	}
+	public Integer getU_nombre_retweet() {
+		return u_nombre_retweet;
+	}
+	public void setU_nombre_retweet(Integer u_nombre_retweet) {
+		this.u_nombre_retweet = u_nombre_retweet;
+	}
+	public Integer getU_nombre_mention() {
+		return u_nombre_mention;
+	}
+	public void setU_nombre_mention(Integer u_nombre_mention) {
+		this.u_nombre_mention = u_nombre_mention;
+	} 
+	
+	
+}
