596347ab264d83a70588f3ad203d75c0587d0937
Clément Le Padellec <Clment Le Padellec@DESKTOP-0DLB6DD>
2020-01-05 15:18:32 +0100

    j'espère pas tout casser le projet avec ce commit and push

--- MesTweets/src/BaseDeTweets.java
+++ MesTweets/src/BaseDeTweets.java
@@ -10,35 +10,167 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.io.Serializable;
+
 public class BaseDeTweets{
 
-//test charlie 
 	public TreeSet<tweet> maCollec;
-
+	public String f_mois = "" ;
+	public String f_semaine = "" ;
+	public String f_jour = "" ;
+	
 	public void initialise() {
 		maCollec = new TreeSet<tweet>();
+	}
+
+	public String getF_mois() {
+		return f_mois;
+	}
+
+	public void setF_mois(String f_mois) {
+		this.f_mois = f_mois;
+	}
+
+	public String getF_semaine() {
+		return f_semaine;
+	}
+
+	public void setF_semaine(String f_semaine) {
+		this.f_semaine = f_semaine;
+	}
+
+	public String getF_jour() {
+		return f_jour;
+	}
+
+	public void setF_jour(String f_jour) {
+		this.f_jour = f_jour;
 	}
 
 	public void ajoute(tweet t) {
 		maCollec.add(t);
 	}
-
-
+	
+	//fonction qui permet de gerer les filtres a appliquer syr le barchart
+	public BaseDeNombreTweet creer_donnee_barchart() {
+		Iterator  it=maCollec.iterator();
+		BaseDeNombreTweet bdnt = new BaseDeNombreTweet();
+		Integer compteur = 0;
+		String modalite = "";
+		
+		/*si il y a un filtre sur les jours */
+		if (f_jour.equals("") == false) {
+				
+			while (it.hasNext()) {
+				tweet infoTweet = (tweet)it.next();
+	
+				if (f_jour.equals(infoTweet.getJour_t()) && f_mois.equals(infoTweet.getMois_t())) {
+				
+					String[] split_h = infoTweet.getHeure().split(":");
+					String heure = split_h[0];
+					
+					if (modalite.equals(heure) || modalite.equals("")) {
+						compteur +=1;
+						modalite = heure;
+					} else {
+						nombreTweet donnee_barchart = new nombreTweet(modalite, compteur);
+						System.out.println(heure + " != " + modalite + " : " + compteur);
+						bdnt.ajoute(donnee_barchart);
+						modalite = heure;
+						compteur = 1;
+					}
+				}
+			}
+		
+		} else {
+			/*si il y a un filtre sur les Semaines */
+			/*
+			if (f_semaine.equals("") == false) {
+				
+				while (it.hasNext()) {
+					tweet infoTweet = (tweet)it.next();
+					if (f_mois.equals(infoTweet.getSemaine_t())){
+						if (modalite.equals(infoTweet.getJour_t()) || modalite.equals("")) {
+							compteur +=1;
+							modalite = infoTweet.getJour_t();
+						} else {
+							nombreTweet donnee_barchart = new nombreTweet(modalite, compteur);
+							System.out.println(infoTweet.getJour_t() + " != " + modalite + " : " + compteur);
+							bdnt.ajoute(donnee_barchart);
+							modalite = infoTweet.getJour_t();
+							compteur = 1;
+						}
+					}
+				}
+			} else {
+			*/	
+				/*si il y a un filtre sur les mois */
+				if (f_mois.equals("") == false) {
+					
+					while (it.hasNext()) {
+						tweet infoTweet = (tweet)it.next();
+						
+						if (f_mois.equals(infoTweet.getMois_t())){
+							if (modalite.equals(infoTweet.getJour_t()) || modalite.equals("")) {
+								compteur +=1;
+								modalite = infoTweet.getJour_t();
+							} else {
+								nombreTweet donnee_barchart = new nombreTweet(modalite, compteur);
+								System.out.println(infoTweet.getJour_t() + " != " + modalite + " : " + compteur);
+								bdnt.ajoute(donnee_barchart);
+								modalite = infoTweet.getJour_t();
+								compteur = 1;
+							}
+						}
+					}
+					
+				} else {
+					/*si il n'y a pas de filtres */
+					while (it.hasNext()) {
+						tweet infoTweet = (tweet)it.next();
+						
+						if (f_mois.equals(infoTweet.getMois_t())){
+							if (modalite.equals(infoTweet.getJour_t()) || modalite.equals("")) {
+								compteur +=1;
+								modalite = infoTweet.getJour_t();
+							} else {
+								nombreTweet donnee_barchart = new nombreTweet(modalite, compteur);
+								System.out.println(infoTweet.getJour_t() + " != " + modalite + " : " + compteur);
+								bdnt.ajoute(donnee_barchart);
+								modalite = infoTweet.getJour_t();
+								compteur = 1;
+							}
+						}
+					}
+				}
+			//}
+		}
+	
+		nombreTweet donnee_barchart = new nombreTweet(modalite, compteur);
+		bdnt.ajoute(donnee_barchart);
+		return bdnt;
+	}
+	
+	
+	
 	public void explore(int i) {
 		Iterator  iterator=maCollec.iterator();
-
+		
 		while (iterator.hasNext())
 		{
-			System.out.println("tweet n° "+ i + " :"+iterator.next());
+			System.out.println("tweet n° "+ i + " :" + iterator.next());
 			i=i+1;
 		}
 	}
-	public static int compterOccurrences(String maChaine, char recherche)
+	
+	public static int compterOccurrences(String maChaine, char recherche) // pompé internet
 	{
 	 int nb = 0;
 	 for (int i=0; i < maChaine.length(); i++)
@@ -85,9 +217,17 @@ public class BaseDeTweets{
 			int nb_hashtag=0;
 			int nb_pseudo_mentionne=0;
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-			String[] sepdate = date.split(" ");
-			String heure_t = sepdate[1];
-			LocalDate date_t = LocalDate.parse(sepdate[0], formatter);
+			String[] sepdate_h = date.split(" ");
+			String heure_t = sepdate_h[1];
+			LocalDate date_t = LocalDate.parse(sepdate_h[0], formatter);
+			//cette variable sert uniquement a séparer les jours mois et annee
+			String date_string = sepdate_h[0];
+			String[] sepdate_j = date_string.split("-");
+			String annee_t = sepdate_j[0];
+			String mois_t = sepdate_j[1];
+			//String jourSemaine_t = sepdate_j[];
+			String jour_t = sepdate_j[2];
+			
 			boolean exist = tweet.contains("#");
 			if(exist==true) {
 				char c= '#';
@@ -152,17 +292,19 @@ public class BaseDeTweets{
 			}else {
 				lien_dans_tweet="non";
 			}
+
+			
 			try {
 				String pseudo_retweet = sepligne[4];
-				tweet t = new tweet(pseudo_u,date_t,heure_t,tweet,lien_dans_tweet,hashtag,pseudo_mentionne,pseudo_retweet,nb_hashtag,nb_pseudo_mentionne);
-				System.out.println("tweet n°"+i+" :"+t+"\n");
+				tweet t = new tweet(pseudo_u,date_t,heure_t,tweet,lien_dans_tweet,hashtag,pseudo_mentionne,pseudo_retweet,nb_hashtag,nb_pseudo_mentionne,annee_t,mois_t,jour_t);
+				System.out.println("tweet nÂ°"+i+" :"+t+"\n");
 				i=i+1;
 				maCollec.add(t);
 
 			} catch (Exception ex){
 				String pseudo_retweet = "aucun pseudo rt";
-				tweet t = new tweet(pseudo_u,date_t,heure_t,tweet,lien_dans_tweet,hashtag,pseudo_mentionne,pseudo_retweet,nb_hashtag,nb_pseudo_mentionne);
-				System.out.println("tweet n°"+i+" :"+t+"\n");
+				tweet t = new tweet(pseudo_u,date_t,heure_t,tweet,lien_dans_tweet,hashtag,pseudo_mentionne,pseudo_retweet,nb_hashtag,nb_pseudo_mentionne,annee_t,mois_t,jour_t);
+				System.out.println("tweet nÂ°"+i+" :"+t+"\n");
 				i=i+1;
 				maCollec.add(t);
 				//System.out.println("pas de retweet");
--- MesTweets/src/Fenetre.java
+++ MesTweets/src/Fenetre.java
@@ -4,21 +4,34 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Separator;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -26,87 +39,350 @@ import javafx.stage.Stage;
 /* clic droit mes news/properties/ java build path/ libraries/ access rules / edit / javafx.** puis accessible /apply */
 
 public class Fenetre extends Application {
+	
+	public BaseDeNombreTweet bar;
+	static BaseDeTweets bdt = new BaseDeTweets();
+		
+	public static void main(String[] args) throws Exception
+	{
+		System.setProperty( "file.encoding", "UTF-8" );
+		/*
+		 * truc a faire :
+		 * Variable semaine
+		 * Gerer les types month et day of week pour eviter de mettre des String 
+		 * Gerer l'affichage des filtre avec les choiceBox de merde
+		 * Reli les données a l'affichage de tableau
+		 * Améliorer l'interface parce que c'ets moche
+		 * Trouver des idées pour les hashtag
+		 * Ajouter des petits truc sympa sur l'affichage des graphiques
+		 * Tester sur les deux jeux de données
+		 * 
+		 */
+		
+		
+		
+		
+		int i=0;
+		//try {
+			System.out.println("Ouverture en cours, veuillez patienter quelques instants");
+			//bdt.ouvrir(i);
+			//bdt.explore(i);
+			//bdt.moisDate();
+		//}catch(Exception ex){
+		//	System.out.println("Importation et enregistrement de la base en cours, veuillez patienter quelques instants...");
+			//bdt.initialise();
+			//bdt.importation("Foot.txt");
+			//bdt.enregistrer();
+			//bdt.explore(i);
+		//}
 
-//
-
-
-	public Scene construitScene() {
-
-		GridPane grid = new GridPane();
-		MenuBar menuBar = new MenuBar();
-		Menu menu1 = new Menu("Menu1");
-		Menu menu2 = new Menu("Menu2");
-		menuBar.getMenus().addAll(menu1,menu2);
-		MenuItem menuItem1 = new MenuItem("MenuItem1");
-		menu1.getItems().addAll(menuItem1,new MenuItem("MenuItem2"));
-
-		Label label = new Label("Label : ");
-		Label label2 = new Label("Label2 : ");
-
-		TextField textField = new TextField();
-		textField.setText("champ texte");
-		TextField textField2 = new TextField();
-		textField2.setText("champ texte 2");
-
-		Button creer = new Button("Cr�er");
-		creer.setText("Cr�er");
-		HBox hBox = new HBox(5);
-		hBox.getChildren().addAll(label, textField, creer);
-
-		Button sauvegarder = new Button("sauvegarder");
-		sauvegarder.setText("sauvegarder");
-		HBox hBox2 = new HBox(10);
-		hBox2.getChildren().addAll(label2, textField2, sauvegarder);
-
-		Text text = new Text("Mon premier texte");
-		Separator separator = new Separator();
-		GridPane.setConstraints(menuBar, 0, 0);
-		grid.getChildren().add(menuBar);
-		GridPane.setConstraints(hBox, 0, 1);
-		grid.getChildren().add(hBox);
-		GridPane.setConstraints(hBox2, 0, 3);
-		grid.getChildren().add(hBox2);
-
-
-
-		GridPane.setConstraints(separator, 0, 2);
-		grid.getChildren().add(separator);
-		GridPane.setConstraints(text, 0, 4);
-		grid.getChildren().add(text);
-		StackPane root = new StackPane();
-		root.getChildren().addAll(grid);
-		Scene scene = new Scene(root, 500, 300);
-		// Scene scene = new Scene(root);
-		return scene;
-
+		launch(args);
 	}
+	
 	public void start(Stage primaryStage)
 	{
+		
 		Stage myStage = primaryStage;
-		primaryStage.setTitle("Ma premiere fenetre");
+		primaryStage.setTitle("Ma première fenêtre");
 		primaryStage.setScene(construitScene());
 		primaryStage.sizeToScene();
 		primaryStage.show();
 	}
-	public static void main(String[] args) throws Exception
-	{
-		System.setProperty( "file.encoding", "UTF-8" );
-		BaseDeTweets bdt = new BaseDeTweets();
-		int i=0;
-		try {
-			System.out.println("Ouverture en cours, veuillez patienter quelques instants");
-			bdt.ouvrir(i);
-			bdt.explore(i);
-		}catch(Exception ex){
-			System.out.println("Importation et enregistrement de la base en cours, veuillez patienter quelques instants...");
-			bdt.initialise();
-			bdt.importation("Foot.txt");
-			bdt.enregistrer();
-			bdt.explore(i);
-		}
+	
+	public Scene construitScene() {
+	
+	    GridPane grid = new GridPane();
+		GridPane grid_contenue = new GridPane();
+		MenuBar menuBar = new MenuBar();
+		
+		HBox hbox_menu = new HBox();
+		VBox vbox_filtre_graph = new VBox();
+		VBox vbox_filtre_users = new VBox();
+		
 
-		launch(args);
+		HBox hbox_filtre_graph_mois = new HBox();
+		HBox hbox_filtre_graph_semaine = new HBox();
+		HBox hbox_filtre_graph_jour = new HBox();
+
+		
+		//création du menu
+		Menu menu_edition = new Menu("Edition");
+		menuBar.getMenus().addAll(menu_edition);
+		
+		//On ajoute des sous menu
+		MenuItem menuItem_tweet = new MenuItem("Tweet");
+		MenuItem menuItem_utilisateur = new MenuItem("Utilisateur");
+		MenuItem menuItem_hashtag = new MenuItem("Hashtags");
+		menu_edition.getItems().addAll(menuItem_tweet, menuItem_utilisateur, menuItem_hashtag);
+
+		hbox_menu.getChildren().add(menuBar);
+		
+		
+		
+		
+		
+		/*************** Tableau ********************/
+		
+		TableView<utilisateur> userTwitter = new TableView<>();
+		
+		TableColumn<utilisateur, String> pseudoColumn = new TableColumn<>("Utilisateur");
+		pseudoColumn.setCellValueFactory(new PropertyValueFactory<>("u_pseudo_users"));
+		TableColumn<utilisateur, Integer> nombreRTColumn = new TableColumn<>("Nombre de Rt");
+		nombreRTColumn.setCellValueFactory(new PropertyValueFactory<>("u_nombre_retweet"));
+		TableColumn<utilisateur, Integer> nombreMentionColumn = new TableColumn<>("Nombre de mention");
+		nombreMentionColumn.setCellValueFactory(new PropertyValueFactory<>("u_nombre_mention"));
+		
+		userTwitter.setItems(getUsers());
+		userTwitter.getColumns().addAll(pseudoColumn, nombreRTColumn, nombreMentionColumn);
+		
+		
+		
+	
+		
+		/****************** ChoiceBox ********************/
+		//Rendre tout ca dynamique 
+		ChoiceBox<String> choiceBox_heure = new ChoiceBox<>();
+		choiceBox_heure.getItems().addAll("Aucun","0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23");
+		//choiceBox_heure.setValue(0);
+		
+		ChoiceBox<String> choiceBox_mois = new ChoiceBox<>();
+		choiceBox_mois.getItems().addAll("Aucun","06","07");
+		choiceBox_mois.setValue("Aucun");
+		
+		ChoiceBox<String> choiceBox_semaine = new ChoiceBox<>();
+		choiceBox_semaine.getItems().addAll("Aucun","26","27","28");
+		
+		ChoiceBox<String> choiceBox_jour = new ChoiceBox<>();
+		choiceBox_jour.getItems().addAll("Aucun","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31");
+
+		Button button_supprimer_mois = new Button("Supprimer");
+		Button button_supprimer_semaine = new Button("Supprimer");
+		Button button_supprimer_jour = new Button("Supprimer");
+		Label label_mois = new Label("Mois : ");
+		Label label_semaine = new Label("Semaine : ");
+		Label label_jour = new Label("Jour : ");
+		hbox_filtre_graph_mois.getChildren().addAll(label_mois,choiceBox_mois);
+		hbox_filtre_graph_semaine.getChildren().addAll();
+		hbox_filtre_graph_jour.getChildren().addAll();
+		
+		vbox_filtre_graph.getChildren().addAll(hbox_filtre_graph_mois, hbox_filtre_graph_semaine, hbox_filtre_graph_jour);
+		
+		
+		vbox_filtre_users.getChildren().add(choiceBox_heure);	
+		GridPane.setConstraints(vbox_filtre_users, 0, 0);
+		GridPane.setConstraints(userTwitter, 0, 1);
+		grid_contenue.getChildren().addAll(userTwitter,vbox_filtre_users);
+		
+		// barchart
+		CategoryAxis xAxis = new CategoryAxis();
+		xAxis.setLabel("Utilisateur");
+
+		NumberAxis yAxis = new NumberAxis();
+		yAxis.setLabel("Nombre de Rt");
+
+		BarChart<String, Number> barChart = new BarChart<String, Number>(xAxis, yAxis);
+		
+		choiceBox_mois.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent m) {
+				
+				//mois
+				if (hbox_filtre_graph_mois.getChildren().contains(button_supprimer_mois) == false) {
+					hbox_filtre_graph_mois.getChildren().add(button_supprimer_mois);
+				}
+				
+				//semaine
+				
+				hbox_filtre_graph_semaine.getChildren().clear();
+				hbox_filtre_graph_semaine.getChildren().addAll(label_semaine,choiceBox_semaine,button_supprimer_semaine);
+				choiceBox_semaine.setValue("Aucun");
+				
+				//jour
+				hbox_filtre_graph_jour.getChildren().clear();
+								
+				
+				//ici on applique les filtres
+				bdt.setF_jour("");
+				bdt.setF_semaine("");
+				bdt.setF_mois(choiceBox_mois.getValue());
+				//ici on créé l'objet barchart et on affiche le graphiques
+				//graph(barChart);
+		    }
+		});
+		
+		choiceBox_semaine.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent s) {
+				
+				hbox_filtre_graph_jour.getChildren().clear();
+				hbox_filtre_graph_jour.getChildren().addAll(label_jour, choiceBox_jour, button_supprimer_jour);
+				choiceBox_jour.setValue("Aucun");
+				
+				bdt.setF_jour("");
+				bdt.setF_semaine(choiceBox_semaine.getValue());
+				//graph(barChart);
+		    }
+		});
+		
+		choiceBox_jour.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent j) {				
+				bdt.setF_jour(choiceBox_jour.getValue());
+				//graph(barChart);
+		    }
+		});
+		
+		
+		
+		button_supprimer_mois.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent s_m) {
+
+				hbox_filtre_graph_mois.getChildren().clear();
+				hbox_filtre_graph_semaine.getChildren().clear();
+				hbox_filtre_graph_jour.getChildren().clear();
+				
+				hbox_filtre_graph_mois.getChildren().addAll(label_mois,choiceBox_mois);
+				choiceBox_mois.setValue("Aucun");
+				
+				bdt.setF_mois("");
+				bdt.setF_semaine("");
+				bdt.setF_jour("");
+			}
+		});
+		
+		button_supprimer_semaine.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent s_s) {
+				hbox_filtre_graph_semaine.getChildren().clear();
+				hbox_filtre_graph_jour.getChildren().clear();
+				
+				hbox_filtre_graph_semaine.getChildren().addAll(label_semaine,choiceBox_semaine);
+				choiceBox_semaine.setValue("Aucun");
+				//vbox_filtre_graph.getChildren().addAll(hbox_filtre_graph_mois,hbox_filtre_graph_semaine,hbox_filtre_graph_jour);
+				
+				bdt.setF_semaine("");
+				bdt.setF_jour("");
+			}
+		});
+		
+		button_supprimer_jour.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent s_j) {
+				
+				hbox_filtre_graph_jour.getChildren().clear();
+				hbox_filtre_graph_jour.getChildren().addAll(label_jour,choiceBox_jour);
+				choiceBox_jour.setValue("Aucun");
+				
+				bdt.setF_jour("");
+			}
+		});
+		
+		
+		
+		/**************MENU**************/
+		
+		menuItem_tweet.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent me) {
+				grid_contenue.getChildren().clear();
+				
+				GridPane.setConstraints(vbox_filtre_graph, 0, 0);
+				grid_contenue.getChildren().add(vbox_filtre_graph);
+
+				GridPane.setConstraints(barChart, 0, 1);
+				grid_contenue.getChildren().add(barChart);
+				
+				//graph(barChart);
+			}
+		});
+		
+		menuItem_utilisateur.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent me) {
+				grid_contenue.getChildren().clear();
+
+				GridPane.setConstraints(vbox_filtre_users, 0, 0);
+				grid_contenue.getChildren().add(vbox_filtre_users);
+
+				GridPane.setConstraints(userTwitter, 0, 1);
+				grid_contenue.getChildren().add(userTwitter);
+			}
+		});
+		
+		menuItem_hashtag.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent me) {
+				grid_contenue.getChildren().clear();
+				GridPane.setConstraints(userTwitter, 0, 0);
+				grid_contenue.getChildren().add(userTwitter);
+			}
+		});
+		
+		
+		
+		
+		
+		//On affiche l'entete et le contenue
+		GridPane.setConstraints(hbox_menu, 0, 0);
+		GridPane.setConstraints(grid_contenue, 0, 1);
+		grid.getChildren().addAll(hbox_menu, grid_contenue);
+		
+		//root prend tout les element de grid
+		StackPane root = new StackPane();
+		root.getChildren().add(grid);
+		
+		//la fenetre avec ses dimensions
+		Scene scene = new Scene(root, 500, 300);
+		
+		return scene;
 	}
+
+	/*ca peut etre utile pour generer dynamiquement les choicebox*/
+	public ChoiceBox<String> setChoiceBox(){
+		ChoiceBox<String> choiceBox = new ChoiceBox<>();
+		
+		choiceBox.getItems().addAll("France","USA","Allemagne");
+		choiceBox.setValue("France");
+		
+		return choiceBox;
+	}
+	
+
+	/*Fonction pour les graphiques*/
+	public void graph(BarChart<String, Number> barChart) {
+		bar = bdt.creer_donnee_barchart();
+		/*Objet permettant d'alimenter les données du barchart*/
+		barChart.getData().clear();
+		barChart.getData().add(bar.remplir_donnee());
+		barChart.setTitle("Nombre de tweet par semaines");
+	}
+	
+
+	/*Fonction pour les tableaus*/
+	public ObservableList<utilisateur> getUsers(){
+		ObservableList<utilisateur> topUsers = FXCollections.observableArrayList();
+		utilisateur utilisateur1 = new utilisateur("chachalartiste",19,20);
+		utilisateur utilisateur2 = new utilisateur("charpogo",77,5);
+		utilisateur utilisateur3 = new utilisateur("mathieuVDP",15,23);
+		topUsers.addAll(utilisateur1,utilisateur2,utilisateur3);
+		return topUsers;
+	}
+	
+	
+	
+	
+	
+	/*public Integer compteOccurence(ArrayList<Integer> filtres, ArrayList<nombreTweet> nombreTweet){
+		Integer cpt = 0;
+		
+		for (int i=0; i<nombreTweet.size(); i++) {
+			nombreTweet tweet = (nombreTweet)(nombreTweet.get(i));
+			
+			for (int j=0; j<filtres.size(); j++){
+				Integer filtre = (Integer)(filtres.get(j));
+				
+				if(filtre.contains(tweet.getMois())) {
+					System.out.println(tweet.getSemaine());
+					cpt+=1;
+				}
+			}
+			
+		}
+		return cpt;
+	}*/
+	
 	public void setVisible(boolean b) {
 		// TODO Auto-generated method stub
 
